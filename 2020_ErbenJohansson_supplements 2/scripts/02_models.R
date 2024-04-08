7c5bb37d9678a4d13382c90e7562fd50070c0ef6
Frederic Blum <frederic_blum@eva.mpg.de>
2024-04-08 16:33:49 +0200

    update

--- 2020_ErbenJohansson_supplements 2/scripts/02_models.R
+++ 2020_ErbenJohansson_supplements 2/scripts/02_models.R
@@ -278,7 +278,7 @@ for (myvar in variables) {
             axis.text.y=element_blank(),
             axis.ticks.y=element_blank(),
             legend.position='none')
-    png(filename=paste0(folder_fig, '/fit_', myvar, '.png'), type='cairo')
+    png(filename=paste0(folder_fig, '/fit_', myvar, '.png'))
     dev.off()
   }
 
@@ -329,7 +329,7 @@ for (myvar in variables) {
             axis.text.y=element_blank(),
             axis.ticks.y=element_blank(),
             legend.position='none')
-    png(filename=paste0(folder_fig, '/fit_', myvar, '.png'), type='cairo') 
+    png(filename=paste0(folder_fig, '/fit_', myvar, '.png')) 
     dev.off()
   }
 
@@ -431,7 +431,7 @@ for (myvar in variables) {
             axis.text.y=element_blank(),
             axis.ticks.y=element_blank(),
             legend.position='none')
-    png(filename=paste0(folder_fig, '/fit_', myvar, '.png'), type='cairo') 
+    png(filename=paste0(folder_fig, '/fit_', myvar, '.png')) 
     dev.off()
 
     ggplot(df_plot_copy, aes(x=word_caps, y=fit, ymin=lwr, ymax=upr, color=dim, label=word_dim)) +
@@ -454,7 +454,7 @@ for (myvar in variables) {
             axis.text.y=element_blank(),
             axis.ticks.y=element_blank(),
             legend.position='none')
-    png(filename=paste0(folder_fig, '/fit_', myvar, '.png'), type='cairo')
+    png(filename=paste0(folder_fig, '/fit_', myvar, '.png'))
     dev.off()
   }
 
