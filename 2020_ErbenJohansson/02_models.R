b3883c50d561f93f9a673b08c47140ccad40f188
Frederic Blum <frederic_blum@eva.mpg.de>
2024-04-12 12:15:15 +0200

    add extreme model

--- 2020_ErbenJohansson/02_models.R
+++ 2020_ErbenJohansson/02_models.R
@@ -17,10 +17,15 @@ set_cmdstan_path(path="/data/tools/stan/cmdstan-2.32.2/")
 # 2: voicing, roundedness
 # 3: height, backness
 
-#                'extreme_roundedness', 'extreme', 'position',
-#                'position_voicing',
 
-myvar <- 'roundedness'
+# To-Do
+# 4: extreme
+# 5: position
+# 8: extreme_roundedness
+# 10: position_voicing
+
+
+myvar <- 'extreme'
 grType=c('cardinal', 'gr35', 'gr60')[1]
 drop_rare_levels=c(TRUE, FALSE)[2]  # drop levels with very few observations (for manner_voicing, unvoiced laterals, vibrants, nasals; for position_voicing, remove voiced glottals)
 
@@ -197,13 +202,18 @@ mod <- brm(
     
     # Intercept for each category
     prior(normal(0, 1), class=Intercept, dpar = 'mu2'),
-    # prior(normal(0, 1), class=Intercept, dpar = 'mu3'),
+    prior(normal(0, 1), class=Intercept, dpar = 'mu3'),
+    prior(normal(0, 1), class=Intercept, dpar = 'mu4'),
     
-    # Standard deviations of intercepts and gp
+    # Standard deviations of intercepts
     prior(exponential(4), class=sd, dpar='mu2'),
-    # prior(exponential(4), class=sd, dpar='mu3'),
+    prior(exponential(4), class=sd, dpar='mu3'),
+    prior(exponential(4), class=sd, dpar='mu4'),
+    
+    # Standard deviations of GP
     prior(exponential(4), class=sdgp, dpar='mu2'),
-    # prior(exponential(4), class=sdgp, dpar='mu3')
+    prior(exponential(4), class=sdgp, dpar='mu3'),
+    prior(exponential(4), class=sdgp, dpar='mu4')
     ),
   silent=0,
   backend='cmdstan',
