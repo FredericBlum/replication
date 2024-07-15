10a8afa5165da756068872b86512dd31a2a3dba4
Frederic Blum <frederic_blum@eva.mpg.de>
2024-07-15 10:17:50 +0200

    update

--- 03_models.R
+++ 03_models.R
@@ -36,10 +36,11 @@ countBy <- function(groupingVar, normBy, dataSource) {
   # Transform counts to proportions
   out <- dataSource %>%
     drop_na(groupingVar) %>% 
-    group_by(wd_id, language, word, concept, family, latitude, longitude, across(groupingVar), across(normBy)) %>%
+    # changing across() to all_of, lets see if this breaks things
+    group_by(wd_id, language, word, concept, family, macroarea, latitude, longitude, across(groupingVar), across(normBy)) %>%
     count() %>% ungroup() %>%
     mutate(prop=n/c_across(normBy), vars=c_across(groupingVar)) %>% 
-    select(language, wd_id, word, concept, family, latitude, longitude, vars, prop) %>%
+    select(language, wd_id, word, concept, family, macroarea, latitude, longitude, vars, prop) %>%
     pivot_wider(names_from=vars, values_from=prop)
   
   # Note: Since we are calculating proportions, this is reasonable!
@@ -50,17 +51,17 @@ countBy <- function(groupingVar, normBy, dataSource) {
 
   # Make sure rows sum up to 1  
   for(i in 1:nrow(out)) {
-    row <- out[i, 8:ncol(out)]
+    row <- out[i, 9:ncol(out)]
     m <- which.max(row[,])
     row[m] <- 1 - sum(row[-m])
-    out[i,] <- cbind(out[i, 1:7], row)
+    out[i,] <- cbind(out[i, 1:8], row)
   }
   
   # Create matrix for brms
   respDir <- as.matrix(out[, myPropVars])
   colnames(respDir) <- 1:ncol(respDir)
   out$respDir <- respDir
-  out <- out[, c('language', 'concept', 'wd_id', 'word', 'family', 'latitude', 'longitude', 'respDir')]
+  out <- out[, c('language', 'concept', 'wd_id', 'word', 'family', 'macroarea', 'latitude', 'longitude', 'respDir')]
 
   return(out)
 }
@@ -72,8 +73,12 @@ df <- read_csv('new_data/data.csv', na=c('')) %>%
   mutate_if(is.character, factor) %>%
   mutate(
     vowelConsonant=ifelse(!is.na(height), 'vowel', 'consonant'),
-    family=fct_na_value_to_level(family, 'Isolate')
-  )
+    family=fct_na_value_to_level(family, 'Isolate'),
+    extreme_roundedness=paste(extreme, roundedness, sep='-'),
+    manner_voicing=paste(manner, voicing, sep='-'),
+    position_voicing=paste(position, voicing, sep='-')
+  ) %>% 
+  filter(!is.na(macroarea))
 
 # Words that are uncommonly long/short
 avg_length <- df %>% group_by(word) %>% summarise(mean=mean(nPhonemesPerWord))
@@ -99,6 +104,10 @@ if (myvar %in% c('manner', 'manner_voicing', 'position', 'position_voicing', 'vo
     ungroup() 
     mv <- 'nVowelsPerWord'
 }
+
+length(unique(df1$language))
+length(unique(df1$family))
+df1 %>% filter(family=='Isolate') %>% group_by(language) %>% count()
 
 myPropVars <- df1 %>% pull(myvar) %>% levels()
 n_levels <- length(myPropVars)
@@ -136,7 +145,7 @@ mod <- brm(
   formula=
     respDir ~
     1 + (1|concept) + (1|language) + (1|family) +
-    gp(longitude, latitude, gr=TRUE),
+    gp(longitude, latitude, gr=TRUE, by=macroarea, scale=F),
   prior=priors,
   silent=0,
   backend='cmdstanr',
