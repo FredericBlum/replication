64bdf0a71de8b2e89503d551b414a1238f6e3a4f
Frederic Blum <frederic_blum@eva.mpg.de>
2025-10-16 08:02:33 +0200

    new format for loo

--- 02_analysis/04_modelComp_loo.r
+++ 02_analysis/04_modelComp_loo.r
@@ -1,13 +1,11 @@
 library(loo)
 library(brms)
 library(tibble)
-library(parallel)
 
-args = commandArgs(trailingOnly=TRUE)
+args=commandArgs(trailingOnly=TRUE)
 
 myvar <- args[1]
-obs <- 1e5
-cores <- detectCores()
+n_sample <- 1e6
 
 # variables <- c(
 #   'voicing', 'roundedness', 'height', 'backness', 'extreme', 'position', 'manner',
@@ -17,18 +15,51 @@ cores <- detectCores()
 path <- 'models/'
 
 # Load models
-with_c <- readRDS(paste0(path, myvar, '.rds'))
 no_c <- readRDS(paste0(path, 'noC_', myvar, '.rds'))
-phylo_c <- readRDS(paste0(path, 'phyloC_', myvar, '.rds'))
-area_c <- readRDS(paste0(path, 'areaC_', myvar, '.rds'))
-
 # Compute LOO
-loo_noC <- loo_subsample(no_c, cores=cores, observations=obs)
-loo_C <- loo_subsample(with_c, cores=cores, observations=obs)
-loo_phyloC <- loo_subsample(phylo_c, cores=cores, observations=obs)
-loo_areaC <- loo_subsample(area_c, cores=cores, observations=obs)
+set.seed(161)
+N <- nobs(no_c)
+idx <- if (N >= 2000) sample.int(N, n_sample) else sample.int(N, max(50, floor(0.05*N)))
+# Index -------------------
 
+loo_noC<- brms::loo_subsample(
+  no_c,
+  observations=idx,
+  ndraws=256,
+  cores=8
+)
+rm(no_c)
+
+area_c <- readRDS(paste0(path, 'areaC_', myvar, '.rds'))
+loo_areaC<- brms::loo_subsample(
+  area_c,
+  observations=idx,
+  ndraws=256,
+  cores=8
+)
+rm(area_c)
+
+phylo_c <- readRDS(paste0(path, 'phyloC_', myvar, '.rds'))
+loo_phyloC<- brms::loo_subsample(
+  phylo_c,
+  observations=idx,
+  ndraws=256,
+  cores=8
+)
+rm(phylo_c)
+
+with_c <- readRDS(paste0(path, myvar, '.rds'))
+loo_withC<- brms::loo_subsample(
+  with_c,
+  observations=idx,
+  ndraws=256,
+  cores=8
+)
+rm(with_c)
+
+
+comp <- loo_compare(loo_noC_voicing, loo_withC_voicing, loo_phyloC_voicing, loo_areaC_voicing)
+print(comp)
 
 # Write comparison to file
-comp <- loo_compare(loo_C, loo_noC, loo_phyloC, loo_areaC)
 write.table(tibble(comp), paste0('models/comp_', myvar), sep="\t", quote=FALSE, row.names=FALSE)
